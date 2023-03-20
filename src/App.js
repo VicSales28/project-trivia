1103d4bc764cfdfe743ee0724f66023585493c2b
Victoria Sales <contatovictoriasales@gmail.com>
2023-03-20 09:18:26 -0300

    Cria btn que leva para settings page (Req.03)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" render={ (props) => <Login { ...props } /> } />
       <Route path="/game" render={ (props) => <Game { ...props } /> } />
+      <Route path="/settings" render={ (props) => <Settings { ...props } /> } />
     </Switch>
   );
 }
